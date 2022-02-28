6e9928fe146eb763de97779307a91cfd5c1ab6f5
LeviWalker <levi.j.walker04@gmail.com>
2022-02-28 17:22:14 -0500

    intake and intake command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,15 @@ package frc.robot;
 public class Constants {
     public static final class OIConstants {
         public static final int squarePS4 = 1;
-        public static final int circlePS4 = 1;
-        public static final int trianglePS4 = 1;
-        public static final int xPS4 = 1;
+        public static final int circlePS4 = 3;
+        public static final int trianglePS4 = 4;
+        public static final int xPS4 = 2;
+
+        public static final int leftXPS4 = 0;
+        public static final int leftYPS4 = 1;
+        public static final int rightXPS4 = 2;
+        public static final int rightYPS4 = 5;
+        public static final int leftTriggerPS4 = 3;
+        public static final int rightTriggerPS4 = 4;
     }
 }
--- src/main/java/frc/robot/intake/commands/IntakeCommand.java
+++ src/main/java/frc/robot/intake/commands/IntakeCommand.java
@@ -4,12 +4,15 @@ import java.lang.reflect.Executable;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.OIConstants;
 import frc.robot.intake.Intake;
 
 public class IntakeCommand extends CommandBase {
 
     private Intake intake;
     private Joystick operator;
+
+    private double intakeAxis;
 
     public IntakeCommand(Intake intake, Joystick operator) {
         this.intake = intake;
@@ -18,6 +21,12 @@ public class IntakeCommand extends CommandBase {
 
     @Override
     public void execute() {
+        intakeAxis = -operator.getRawAxis(OIConstants.rightTriggerPS4);
+        if (Math.abs(intakeAxis) > 0.07 && !intake.isDeployed())
+            intake.deploy();
+        else if (Math.abs(intakeAxis) < 0.07 && intake.isDeployed())
+            intake.retract();
         
+        intake.setIntake(-intakeAxis);
     }
 }
